3d64b250085170848e13908d0451e55899f923b1
hypno7 <abdelrahman.ahmed@live.com>
2015-03-23 10:50:53 +0200

    Checkout, Orders and Pagination

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -1,8 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(function() {
-  return $('.store .entry > img').click(function() {
-    return $(this).parent().find(':submit').click();
-  });
-});
